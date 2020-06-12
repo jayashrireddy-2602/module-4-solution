4b187a1a522d28e89d95f611d5047fb06e1bbffe
Reddy Jayasri <51327445+jayashrireddy-2602@users.noreply.github.com>
2020-06-12 12:21:52 +0530

    Create speakhello.js

---
+++ speakhello.js
@@ -0,0 +1,13 @@
+(function (window) {
+
+var helloSpeaker = {};
+
+
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
